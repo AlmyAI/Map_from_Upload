336304bdfee114b10ccf30b9f1f28bc8561b9ffc
AlmyAI <134756767+AlmyAI@users.noreply.github.com>
2023-07-24 13:23:20 -0500

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+
 st.title("Map from File")
 st.markdown(
   """Upload a **.csv** file that contains **columns named longitude and latitude** and a map will be displayed.
@@ -15,4 +16,6 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.map(df)
     st.dataframe(df)
-
+    if 'dataframe' not in st.session_state:
+      st.session_state.dataframe = df
+      
