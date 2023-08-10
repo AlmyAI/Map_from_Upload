05a672c55ccda1bf585e4e6d38555b2c0a8eca06
AlmyAI <134756767+AlmyAI@users.noreply.github.com>
2023-08-10 14:20:24 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,4 +4,8 @@ from streamlit_elements import elements, mui, html
 
 def set_title(varTitle, varSubtitle):
   st.markdown(f"""# {varTitle} <span style=color:#7ab3ba><font size=5>{varSubtitle}</font></span>""",unsafe_allow_html=True)
+  
+
+def set_instructions():
+  st.markdown("""Upload a **.csv** file that contains **columns named longitude and latitude** and a map will be displayed.""")
   st.divider()
