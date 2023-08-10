a336785c9d4c91ddae8a557e613af579ff6c3aab
AlmyAI <134756767+AlmyAI@users.noreply.github.com>
2023-08-10 14:29:05 -0500

    Update Map.py

--- Map.py
+++ Map.py
@@ -4,8 +4,31 @@ from utils import set_title, set_instructions
 
 
 set_title("Alma Clinical", "Hot Map")
-set_instructions()
 
+st.divider()
+
+main_container = st.container()
+with main_container:
+  st.header("Instructions")
+  instructions_expander = st.expander("Expand/Collapse")
+  
+  with instructions_expander:
+    set_instructions()
+  
+  st.divider()
+
+  tab_all, tab_accent, tab_harmony, tab_duo = st.tabs(["All", "Accent", "Harmony", "Duo"])
+    with tab_all:
+      st.header("All Devices")
+
+    with tab_accent:
+      st.header("Accent")
+
+    with tab_harmony:
+      st.header("Harmony")
+
+    with tab_duo:
+      st.header("Duo")
 
 uploaded_file = st.file_uploader("CSV Uploader", type=['csv'])
 if uploaded_file is not None:
